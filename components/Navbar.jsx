633cc8814bb7dbbccbc0b6dd7958d19e767c9873
manlikexavy <chibokaxavier@gmail.com>
2023-06-09 08:14:13 -0700

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,10 +4,10 @@ import { HiMoon } from "react-icons/hi";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-    const [nav,setNav]= useState(false)
-    const handleNav = ()=>{
-        setNav(!nav);
-    }
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div className="w-[100%] shadow-2xl rounded-lg mx-auto flex items-center justify-between h-20 font-bold px-4">
       <Link href={"/"}>
@@ -28,12 +28,21 @@ const Navbar = () => {
           Sign Up
         </Link>
       </div>
-      <div onClick={handleNav} className="block md:hidden cursor-pointer z-10 mr-4">
-       {nav ?  <AiOutlineClose size={25} />: <AiOutlineMenu size={25} />}
+      <div
+        onClick={handleNav}
+        className="block md:hidden cursor-pointer z-10 mr-4"
+      >
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
       </div>
 
       {/* mobile menu */}
-      <div className={nav?"md:hidden fixed left-0 top-20 flex flex-col justify-between w-full  h-[70%] ease-in duration-300  z-10" : "fixed left-[-100%] top-20 flex flex-col justify-between w-full  h-[70%] ease-in duration-300  z-10 "}>
+      <div
+        className={
+          nav
+            ? "md:hidden fixed left-0 top-20 flex flex-col justify-between w-full  h-[70%] ease-in duration-300  z-10"
+            : "fixed left-[-100%] top-20 flex flex-col justify-between w-full  h-[70%] ease-in duration-300  z-10 "
+        }
+      >
         <ul className="w-full p-4 ">
           <li className="border-b py-6">
             <Link href="/">Home</Link>
