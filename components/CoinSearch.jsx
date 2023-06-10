9c7c411da1de94a96cb0c9d058b5b0ada59964c6
manlikexavy <chibokaxavier@gmail.com>
2023-06-10 07:07:39 -0700

    DISPLAYING SOME DATA NOW, ABOUT TO INSTALL REACT SPARKLINE LIBRARY, STILL WORKING ON THE UI

--- components/CoinSearch.jsx
+++ components/CoinSearch.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
 
-const CoinSearch = ({coins}) => {
+const CoinSearch = ({ coins }) => {
   return (
-    <div>{coins.map((coin)=>(
-        <div key={coin.id}>
-            {coin.name}
-        </div>
-    ))}</div>
-  )
-}
+    <div>
+      <div>
+        <h1>Search for Crypto</h1>
+        <form>
+          {" "}
+          <input type="text" placeholder="Search a Coin" />
+        </form>
+      </div>
 
-export default CoinSearch
+      <div className="grid grid-cols-9">
+        <div></div>
+        <div>#</div>
+        <div>Coin</div>
+        <div></div>
+        <div>Price</div>
+        <div>24h</div>
+        <div>24h Volume</div>
+        <div>Mkt</div>
+        <div>Last 7 Days</div>
+      </div>
+      <div>
+        {coins.map((coin) => (
+          <div className="grid grid-cols-9">
+            <div>
+              <AiOutlineStar />
+            </div>
+            <div>{coin.market_cap_rank}</div>
+            <div>
+              <div className=" ">
+                <img src={coin.image} alt={coin.id} />
+                <p>{coin.name}</p>
+              </div>
+            </div>
+            <div>{coin.symbol}</div>
+            <div>{coin.current_price}</div>
+            <div>{coin.price_change_percentage_24h}</div>
+            <div>{coin.total_volume}</div>
+            <div>{coin.market_cap}</div>
+            <div>{coin.sparkline_in_7d.price}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CoinSearch;
