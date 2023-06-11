df9f23f24d75708d5be4e0841f1e02598096c037
manlikexavy <chibokaxavier@gmail.com>
2023-06-11 08:03:43 -0700

    BOTH APIS WORKING WELL  NOW, ABLE TO FETCHJ DATA FROM BOTH APIS

---
+++ components/Trending.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Trending = ({ trending }) => {
+  console.log(trending);
+  return <div> </div>;
+};
+
+export default Trending;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import CoinSearch from "@/components/CoinSearch";
+import Trending from "@/components/Trending";
 import { useEffect, useState } from "react";
 
 export default function Home({ coins }) {
@@ -6,19 +7,39 @@ export default function Home({ coins }) {
     <main>
       <div className=" min-h-screen  px-4">
         <CoinSearch coins={coins} />
+        <Trending />
       </div>
     </main>
   );
 }
 
 export async function getServerSideProps(context) {
-  const request = await fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ngn&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=en"
-  ).then((res) => res.json());
+  const [requestRes, trendingRes] = await Promise.all ([
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ngn&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=en"
+    ),
+    fetch("https://api.coingecko.com/api/v3/search/trending"),
+  ]);
+  const [request, trending] = await Promise.all ([
+    requestRes.json(),
+    trendingRes.json(),
+  ]);
 
   return {
     props: {
       coins: request,
+      trending: trending.coins,
     },
   };
 }
+// export async function getServerSideProps(context) {
+//   const request = await fetch(
+//     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ngn&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=en"
+//   ).then((res) => res.json());
+
+//   return {
+//     props: {
+//       coins: request,
+//     },
+//   };
+// }
