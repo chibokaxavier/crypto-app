89f38d457ec52ba34e1567ef6c0e8804d2e0787a
manlikexavy <chibokaxavier@gmail.com>
2023-06-13 08:26:01 -0700

    FINISHED THE CONTENTS OF THE FOOTER , NEXT IS TO STYLE IT

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineInstagram } from "react-icons/ai";
+import {
+  FaTwitter,
+  FaFacebook,
+  FaGithub,
+  FaReddit,
+  FaTiktok,
+} from "react-icons/fa";
+import { HiMoon } from "react-icons/hi";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <div>
+        <div>
+          <div>
+            <h2>Support</h2>
+            <ul>
+              <li>Help Center</li>
+              <li>Contact Us</li>
+              <li>API Status</li>
+              <li>Documentation</li>
+            </ul>
+          </div>
+          <div>
+            <h2>Info</h2>
+            <ul>
+              <li>About Us</li>
+              <li>Careers</li>
+              <li>Invest</li>
+              <li>Legal</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>
+              <div>
+                {" "}
+                <HiMoon />{" "}
+              </div>
+              <p>Sign up for crypto news</p>
+              <div>
+                <form action="">
+                  <input type="text" placeholder="Enter your Email" />
+                  <button>Sign in</button>
+                </form>
+              </div>
+              <div>
+                <AiOutlineInstagram />
+                <FaTiktok />
+                <FaTwitter />
+                <FaFacebook />
+                <FaGithub />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="text-center py-4 "> Powered by Coin Gecko</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = ({children}) => {
-
+const Layout = ({ children }) => {
   return (
-    <div className=''>
-        <Navbar/>
-        {children}
-
+    <div className="">
+      <Navbar />
+      {children}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
