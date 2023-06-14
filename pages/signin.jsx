ee8bb6bcc5537614b5822b25ab2ed2e1e6fbcf19
manlikexavy <chibokaxavier@gmail.com>
2023-06-14 00:15:59 -0700

    FINISHED DISPLAYING THE CONTENTS 0F THE SIGN IN PAGE, UP NEXT IS STYLING IT

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { AiFillLock, AiOutlineMail } from "react-icons/ai";
 
 const signin = () => {
   return (
-    <div>signin</div>
-  )
-}
+    <div>
+      <div>
+        <h1>Sign In</h1>
+        <form action="">
+          <div>
+            <label htmlFor="">Email</label>
+            <div>
+              <input type="email" />
+              <AiOutlineMail/>
+            </div>
+          </div>
+          <div>
+            <label htmlFor="">Password</label>
+            <div>
+              <input type="password"/>
+              <AiFillLock/>
+            </div>
+          </div>
+          <button>Sign in</button>
+        </form>
+        <p>Dont't have an account ? <Link href='/signup'>Sign Up </Link> </p>
+      </div>
+    </div>
+  );
+};
 
-export default signin
+export default signin;
